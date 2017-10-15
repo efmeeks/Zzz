58fc16c36de332920dd79acc0dc5efb3143e9f31
Eric Meeks <eric@efmeeks.net>
2017-10-15 17:21:02 -0500

    .

--- README.md
+++ README.md
@@ -1,33 +1,21 @@
-# Zzz
+# zzz
 
-Here's command line tool I made to blank your Mac display without putting the machine to sleep.
+`zzz` is a command line tool to blank your Mac display without putting the machine to sleep.
 
-It is the command line equivalent of <a href="https://support.apple.com/en-us/HT201236" target="new">Control–Shift–Eject</a>.
+It is the cli equivalent of [Control–Shift–Eject](https://support.apple.com/en-us/HT201236).
 
-*Note: This only works on OS X*
+*macOS only*
 
-> #### Why not just press ⌃⇧⏏ ?
->
+> **Why not just press ⌃⇧⏏ ?**
 > If you can physically press the keys, then by all means do that. However, Zzz is intended for applications where the user is not within reach of the physical machine e.g. remote ssh.
 
-## Usage
-
-```
-# Download Zzz.zip
-wget https://github.com/efmeeks/Zzz/archive/master.zip
-
-# Unzip the archive
-unzip Zzz-master.zip
-
-# Open the folder
-cd Zzz-master
-
-# Ensure executability
-chmod +x zzz.sh
-
-Run the script
-./zzz.sh
+## Try
+```bash
+curl -sL file.efmeeks.net/zzz/zzz.sh | bash
 ```
 
-You should see something like this...
-![Screenshot](./zzz-ss.png)
+## Get
+
+```bash
+bash <(curl -sL file.efmeeks.net/zzz/zzz.sh) install
+```
--- zzz.sh
+++ zzz.sh
@@ -1,46 +1,65 @@
 #!/bin/bash
 
-cat << "HEAD"
-  ________  ________  ________
- |\_____  \|\_____  \|\_____  \
-  \|___/  /|\|___/  /|\|___/  /|
-     /  / /    /  / /    /  / /
-    /  /_/__  /  /_/__  /  /_/__
-   |\________\\________\\________\
-    \|_______|\|_______|\|_______|
+# zzz
+# https://git.efmeeks.net/zzz
 
-         2017 Eric Meeks
-         www.efm.io
+help() {
+  cat << eof
 
-Sleeps your Mac display, but not your Mac.
-Equivalent to Control–Shift–Eject.
+  zzz
 
-HEAD
+  Usage
+    zzz [option] command
+  
+  Options
+    [p]ath PATH   | Set $ipath for install command (default: /usr/local/bin)
+  
+  Commands
+    [h]elp        | Show help message
+    [s]leep       | Sleep display (default)
+    [i]nstall     | Make \`zzz\` a command in your \$PATH
+    [u]ninstall   | Remove \`zzz\`
 
-ZZZ=`which zzz`
-
-PS3="Enter 1, 2, 3 or 4: "
-options=("Sleep display now" "Install Zzz" "Uninstall Zzz" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Sleep display now")
-            clear
-            pmset displaysleepnow
-            break
-            ;;
-        "Install Zzz")
-            [ ! -f /usr/local/bin/zzz ] && echo "pmset displaysleepnow; clear" > /usr/local/bin/zzz; chmod +x /usr/local/bin/zzz; echo "Install complete. Run zzz to sleep your display"; break || echo "Zzz already installed"
-            break
-            ;;
-        "Uninstall Zzz")
-            [ -x $ZZZ ] && rm $ZZZ; echo "Uninstall complete."; break || echo "Zzz not currently installed"
-            break
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo "invalid option"
-            ;;
-    esac
-done
+eof
+exit
+}
+setpath() {
+  [ -d "$1" ] && local ipath="$1"
+  install
+}
+zzz() {
+  pmset displaysleepnow
+  clear
+  exit
+}
+install() {
+  if [ -n "$(which zzz)" ]; then
+    echo "zzz is already installed at $(which zzz)"
+  else
+    [ -z "$ipath" ] && ipath="/usr/local/bin"
+    curl -sL file.efmeeks.net/zzz/zzz.sh -o "$ipath/zzz"
+    chmod +x "$ipath/zzz"
+    [ -z "$(which zzz)" ] && echo "Make sure $ipath is in your \$PATH" || echo "zzz installed"
+  fi
+  exit
+}
+uninstall() {
+  upath="$(which zzz)"
+  if [ -z "$upath" ]; then
+    echo "Can't seem to find zzz in your \$PATH"
+    exit
+  else
+    echo "Remove $upath? [y/N]"
+    while read sure; do
+      [ "$sure" == "y" -o "$sure" == "yes" -o -z "$sure" ] && rm "$upath" && echo "zzz uninstalled"
+      exit
+    done
+  fi
+  exit
+}
+[ "$(uname)" != "Darwin" ] && echo "Sorry, this tool is Mac-only." && exit
+[ "$1" == "h" -o "$1" == "help" ] && help
+[ "$1" == "p" -o "$1" == "path" ] && setpath "${@:2}"
+[ "$1" == "i" -o "$1" == "install" ] && install
+[ "$1" == "u" -o "$1" == "uninstall" ] && uninstall
+[ "$1" == "s" -o "$1" == "sleep" ] && zzz || zzz
